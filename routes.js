3e8d7ac045e6f9024151cf1d4c316d5893a74782
Michael Paris <mlparis92@gmail.com>
2013-08-02 19:38:03 -0400

    Routes

--- routes.js
+++ routes.js
@@ -2,5 +2,9 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index');
     });
+
+    app.get('/clock', function(req, res) {
+        res.render('colorclock');
+    });
 };
 
