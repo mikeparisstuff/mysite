ad7c2666920f258b535543cc9e92f7a5b1fc859c
Michael Paris <mlparis92@gmail.com>
2013-08-02 16:01:45 -0400

    Structure update

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = function(app, express) {
+
+    var config = this;
+
+    app.configure(function() {
+        app.set('views', __dirname + '/views');
+        console.log(app.set('views'));
+        app.set('view engine', 'html');
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.engine('html', require('ejs').renderFile);
+        app.use(express.static(__dirname + '/public'));
+    });
+
+    app.configure('development', function(){
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true}));
+    });
+
+    return config;
+};
